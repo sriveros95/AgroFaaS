9ff1e72421be29e2e6fc4134de9384f7ead82bfc
santiago riveros <santiago.riveros@gmail.com>
2019-09-06 16:55:21 -0500

    checking something
    Signed-off-by: santiago riveros <“santiago.riveros@gmail.com”>

--- lunar-agriculture/handler.go
+++ lunar-agriculture/handler.go
@@ -30,26 +30,6 @@ func Handle(req []byte) string {
 	moonString, err := ioutil.ReadAll(resp.Body)
 	fmt.Println(moonString)
 	moonData := strings.Split(string(moonString), ",")
-	switch moonData[1] {
-	case " new (totally dark)", " in its first quarter (increasing to full)":
-		return "'growth' season, considered fertile and wet. All plants producing above ground growth, fruits, or flowers benefit from a waxing moon planting."
-
-	case " waxing gibbous (increasing to full)":
-		return "The waxing period tends to be a very productive time of period for not only planters, but all of society as a whole. Sow/Plant: Above ground annuals, especially Fruit plants also Cereals and Flowers"
-
-	case " waxing crescent (increasing to full)":
-		return "Sow/Plant: Above ground annuals, especially Leaf plants also Cereals, Herbs, Cucumbers"
-
-	case " full (full light)":
-		return "Time to rest, celebrate and meditate"
-
-	case " waning gibbous (decreasing from full)", " in its last quarter (decreasing from full)":
-		return "Sow/Plant: Below ground plants, especially Root plants, Plant trees, shrubs and perennials. Harvest all crops, Fertilize, Transplant, Mow lawns & Prune"
-
-	case " waning crescent (decreasing from full)":
-		return "Barren phase: Time to rest. Avoid seed sowing, Harvest and store crops, Fertilize, Transplant, Destroy weed, Mow lawns & Prune"
-
-	default:
-		return "Unknown phase"
-	}
+	fmt.Println(moonData)
+	return ""
 }
