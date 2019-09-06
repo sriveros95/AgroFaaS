d363f09d6e6b0baebeff52c4482f3adf599998ce
santiago riveros <santiago.riveros@gmail.com>
2019-09-06 17:39:35 -0500

    moon farmer testing comms with moon_phase
    Signed-off-by: santiago riveros <“santiago.riveros@gmail.com”>

--- moon-farmer/handler.go
+++ moon-farmer/handler.go
@@ -1,9 +1,12 @@
 package function
 
 import (
+	"bytes"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strings"
+	"time"
 )
 
 func Handle(w http.ResponseWriter, r *http.Request) {
@@ -17,6 +20,29 @@ func Handle(w http.ResponseWriter, r *http.Request) {
 		input = body
 	}
 
+	moonFuncURL := "http://127.0.0.1:8080/function/sriveros95/moon-phase"
+	earthTime := time.Now()
+	var err interface{}
+	if len(string(input)) != 0 {
+		earthTime, err = time.Parse(time.RFC3339, string(input))
+		if err != nil {
+			// todo log error
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("Failed lunar-agriculture time.Parse"))
+			return
+		}
+	}
+	resp, err := http.Post(moonFuncURL, "string", bytes.NewReader([]byte(earthTime.Format(time.RFC3339))))
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Failed http.Post"))
+		return
+	}
+	defer resp.Body.Close()
+	moonString, err := ioutil.ReadAll(resp.Body)
+	fmt.Println(moonString)
+	moonData := strings.Split(string(moonString), ", ")
+
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(fmt.Sprintf("Hello world, input was: %s", string(input))))
+	w.Write([]byte(fmt.Sprintf("Hello world, input was: %s \n MoonString: %s \n %v", string(input), moonString, moonData)))
 }
